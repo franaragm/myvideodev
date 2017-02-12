dceaea8a0c116e1771683a19f65582e7cbf71a67
franaragm <franaragonmesa@gmail.com>
2017-02-12 17:21:30 +0100

    - Instalación de dependencias para autenticación con tokens y paginación - Configuración de archivos de rutas inicial - Creación de Servicio Helper para la Api REST

---
+++ src/AppBundle/Services/APIRestHelpers.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Services;
+
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class APIRestHelpers
+{
+    /**
+     * Retorna datos en formato JSON
+     *
+     * @param $data
+     * @return Response
+     */
+    public function getjson($data) {
+        $normalizers = array(new GetSetMethodNormalizer());
+        $encoders = array("json" => new JsonEncoder());
+
+        $serializer = new Serializer($normalizers, $encoders);
+        $json = $serializer->serialize($data, 'json');
+
+        $response = new Response();
+        $response->setContent($json);
+        $response->headers->set("Content-Type", "application/json");
+
+        return $response;
+    }
+}
